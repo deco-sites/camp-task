5ffaf158bb0ee186c622624c0fac35b086df81c6
@yuri_assuncx <killerkoll2012@gmail.com>
2024-03-19 21:29:36 -0300

    feat: creating temperatures sections

---
+++ components/ui/TemperatureFloatButton.tsx
@@ -0,0 +1,27 @@
+import { Temperature } from "apps/weather/loaders/temperature.ts";
+
+export interface Props {
+  temperature: Temperature;
+}
+
+function TemperatureFloatButton({ temperature }: Props) {
+  if (!temperature) {
+    return null;
+  }
+
+  const backgroundColor = temperature.celsius < 15
+    ? "from-sky-600 to-sky-800"
+    : "from-orange-600 to-red-800";
+
+  return (
+    <div class="fixed bottom-6 right-6 z-40">
+      <div
+        class={`py-2 px-6 rounded-full shadow-lg bg-gradient-to-b text-white text-2xl ${backgroundColor}`}
+      >
+        {temperature.celsius}°C
+      </div>
+    </div>
+  );
+}
+
+export default TemperatureFloatButton;
---
+++ components/ui/TemperatureText.tsx
@@ -0,0 +1,32 @@
+import { Temperature } from "apps/weather/loaders/temperature.ts";
+
+import type { HTMLWidget } from "apps/admin/widgets.ts";
+
+export interface Props {
+  text: HTMLWidget;
+  position?: "flex-col" | "flex-col-reverse" | "flex-row" | "flex-row-reverse";
+  alignment?: "items-start" | "items-center" | "items-end";
+  temperature: Temperature;
+}
+
+export default function TemperatureText(
+  { text, position = "flex-col", alignment = "items-center", temperature }:
+    Props,
+) {
+  const textColor = temperature.celsius < 15
+    ? "from-blue-600 via-sky-500 to-blue-800"
+    : "from-red-600 via-orange-500 to-red-800";
+
+  return (
+    <div
+      class={`flex justify-center py-6 px-4 container gap-8 ${position} ${alignment}`}
+    >
+      <h1
+        class={`bg-gradient-to-r inline-block text-transparent bg-clip-text text-6xl ${textColor}`}
+      >
+        {temperature.celsius}°C
+      </h1>
+      <div dangerouslySetInnerHTML={{ __html: text || "" }} />
+    </div>
+  );
+}
