4022d864c69afedc645439d50cfaf2f5b7cc3574
@yuri_assuncx <killerkoll2012@gmail.com>
2024-04-23 15:12:16 -0300

    feat (TotalVotes): making a new request every 30 seconds

--- components/ui/TotalVotes.tsx
+++ components/ui/TotalVotes.tsx
@@ -10,20 +10,22 @@ export default function TotalVotes() {
   const loading = useSignal(false);
   const { totalVotes } = useUI();
 
+  async function getTotalVotes() {
+    loading.value = true;
+
+    const votes = await invoke["deco-sites/camp-task"].loaders.camp.votes();
+
+    totalVotes.value = votes.total;
+    loading.value = false;
+  }
+
   useEffect(() => {
-    async function getTotalVotes() {
-      loading.value = true;
-
-      const votes = await invoke["deco-sites/camp-task"].loaders.camp.votes();
-
-      totalVotes.value = votes.total;
-      loading.value = false;
-    }
-
     getTotalVotes();
   }, [totalVotes.value]);
 
   if (loading.value) return <div class="loading loading-spinner" />;
+
+  setInterval(getTotalVotes, 30 * 1000);
 
   return (
     <div
